f563069684c6bfe30e646344f85830090b9e4e2f
juniorMartinss <reginaldo.jmsilva@outlook.com>
2022-07-10 16:52:16 -0300

    Atualização do exercicio 010

--- 010.py
+++ 010.py
@@ -1,1 +1,5 @@
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. (considere  US$ 1,00 = 3,27
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. (considere  US$ 1,00 = 5,26 (cotação 10/07/2022)
+
+din = float(input('Qual valor você tem na sua carteira? '))
+dol = din / 5.26
+print('O valor que você pode comprar em dólares são: ${:.2f}'.format(dol))
