08167f21c5191904563026802a33a63984b5f3ed
juniorMartinss <reginaldo.jmsilva@outlook.com>
2022-07-12 17:04:05 -0300

    Atualização do exercicio do 012

--- 012.py
+++ 012.py
@@ -3,4 +3,4 @@
 val = float(input('Digite o valor do produto: '))
 nval = val * 0.05
 desc = val - nval
-print('O novo valor do produto que era {}, acresentando 5% é: {}'.format(val, desc))
+print('O novo valor do produto que era {}, acresentando 5% é: {:.2f}'.format(val, desc))
