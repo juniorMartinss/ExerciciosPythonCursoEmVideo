d583f19f068d23225e475f4bd90bf9eb585818ea
juniorMartinss <reginaldo.jmsilva@outlook.com>
2022-07-12 17:00:09 -0300

    Atualização do exercicio dos exercicios 008, 011, 012, 013

--- 009.py
+++ 009.py
@@ -12,4 +12,4 @@ t8 = n * 8
 t9 = n * 9
 t10 = n * 10
 
-print('A tabuata do número {} é: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(n, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10))
+print('A tabuata do número {} é:\n {} x  1 = {}\n {} x  2 = {}\n {} x  3 = {}\n {} x  4 = {}\n {} x  5 = {}\n {} x  6 = {}\n {} x  7 = {}\n {} x  8 = {}\n {} x  9 = {}\n {} x 10 = {}'.format(n, n, t1, n, t2, n, t3, n, t4, n, t5, n, t6, n, t7, n, t8, n, t9, n, t10))
--- 012.py
+++ 012.py
@@ -1,6 +1,6 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 val = float(input('Digite o valor do produto: '))
-nval = val * 1.05
-
-print('O novo valor do produto que era {}, acresentando 5% é: {}'.format(val, nval))
+nval = val * 0.05
+desc = val - nval
+print('O novo valor do produto que era {}, acresentando 5% é: {}'.format(val, desc))
