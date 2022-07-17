87b987a43535249eaa303e26b0eba2883c8e5c1a
juniorMartinss <reginaldo.jmsilva@outlook.com>
2022-07-17 18:33:40 -0300

    Renomeando 001 e 002

--- 021.py
+++ 021.py
@@ -1,1 +1,9 @@
 #Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+#feito pelo prof°
+import pygame
+pygame.init()
+pygame.mixer.music.load('021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
