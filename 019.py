b57affa551870bc190569417b28909bd0dd23e6e
juniorMartinss <reginaldo.jmsilva@outlook.com>
2022-07-17 18:41:13 -0300

    Exercicios do 016 ao 021

--- 019.py
+++ 019.py
@@ -1,5 +1,7 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
 # nome deles e escrevendo o nome do escolhido.
+print('*' * 51)
+
 from random import choice
 
 nome = input('Digite o nome do primeiro aluno(a): ')
@@ -11,4 +13,6 @@ lista = [nome, nome2, nome3, nome4]
 sorteio = choice(lista)
 
 print('Os particpantes da disputa para apagar o quadro são {}, {}, {}, {} e o sorteado(a) é:\n{}, Parabéns!!! Você vai apagar o quadro. '.format(nome, nome2, nome3, nome4, sorteio))
+
+print('*' * 51)
 
